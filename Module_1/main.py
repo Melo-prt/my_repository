8285e711d2b53be6202d18e07afeae433281455e
Melo_prt <egorprotskoo@ya.ru>
2024-11-17 19:12:21 +0300

    ПЗ "Базовые структуры данных" 17.11.2024

---
+++ Module_1/main.py
@@ -0,0 +1,18 @@
+# 1st program
+print((9 ** 0.5) * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+x1 = 2 * 2 + 2
+print(x1)
+x2 = 2 * (2 + 2)
+print(x2)
+print(x1 == x2)
+
+# 4th program
+str_ = '123.456'
+float_ = float(str_) * 10
+int_ = int(float_)
+print(int_ % 10)
